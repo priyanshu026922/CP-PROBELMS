d8876e504ed5640f1e8919a8052f12e52d068c15
Priyanshu Ranjan <priyanshuranjan7856@gmail.com>
2025-07-14 00:05:54 +0530

    new ques

---
+++ B_Roof_Construction.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef map<char, int> mci;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define maxe(v) (*max_element(all(v)))
+#define mine(v) (*min_element(all(v)))
+#define sumv(v) (accumulate(all(v), 0LL))
+#define sortasc(v) sort(all(v))
+#define sortdesc(v) sort(rall(v))
+#define lb(v, x) (lower_bound(all(v), x) - (v).begin())
+#define ub(v, x) (upper_bound(all(v), x) - (v).begin())
+#define ispresent(v, x) (binary_search(all(v), x))
+#define freqmap(mp, v) for (auto &el : v) mp[el]++
+
+
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define repi(i, a, b) for (int i = (a); i <=(b); ++i)
+#define repr(i, a, b) for (int i = (a); i >= (b); --i)
+#define ff first
+#define ss second
+// #################OA AMAZON @2#####
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+bool cmp(pair<ll,ll>&a,pair<ll,ll>&b){
+  if(a.first!=b.first){
+    return a.ff>b.ff;
+  }
+  return a.ss<b.ss;
+}
+
+void solve() {
+ll n;
+cin>>n;
+vll a;
+n--;
+
+ll msb=log2(n);
+ll num=pow(2,msb)-1;
+while(num>=0){
+a.push_back(num);
+num--;
+}
+num=pow(2,msb);
+while(num<=n){
+a.push_back(num);
+num++;
+}
+
+rep(i,0,n){
+    cout<<a[i]<<" ";
+}
+cout<<endl;
+}
+
+int main() {
+    fastio();
+    int n;
+    cin >> n;
+    while (n--) {
+        solve();
+    }
+    return 0;
+}
+
+
+
---
+++ Chef_Bakes_Cake_1.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ------------------ Type Aliases ------------------
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef map<int, int> mii;
+
+// ------------------ STL Helpers ------------------
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define maxe(v) (*max_element(all(v)))
+#define mine(v) (*min_element(all(v)))
+#define sumv(v) (accumulate(all(v), 0LL))
+#define sortasc(v) sort(all(v))
+#define sortdesc(v) sort(rall(v))
+#define lb(v, x) (lower_bound(all(v), x) - (v).begin())
+#define ub(v, x) (upper_bound(all(v), x) - (v).begin())
+#define ispresent(v, x) (binary_search(all(v), x))
+#define freqmap(mp, v) for (auto &el : v) mp[el]++
+
+// ------------------ Looping ------------------
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define repr(i, a, b) for (int i = (a); i >= (b); --i)
+#define ff first
+#define ss second
+
+// ------------------ Fast I/O ------------------
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+// ------------------ Solve Function ------------------
+void solve() {
+   
+    
+}
+
+
+    
+
+// ------------------ Main Function ------------------
+int main() {
+    fastio();
+   int n,m;
+   cin>>n>>m;
+   cout<<m*50-n*30<<endl;
+}
