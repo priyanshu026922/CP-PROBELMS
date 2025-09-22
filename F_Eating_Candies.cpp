1c23f261c95405c57cb6c1be49a484b7e7aba665
priyanshu026922 <btech10886.23@bitmesra.ac.in>
2025-09-22 18:19:50 +0530

    CF1100

---
+++ F_Eating_Candies.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define maxe(v) (*max_element(all(v)))
+#define mine(v) (*min_element(all(v)))
+#define sumv(v) (accumulate(all(v), 0LL))
+#define lb(v, x) (lower_bound(all(v), x) - (v).begin())
+#define ub(v, x) (upper_bound(all(v), x) - (v).begin())
+#define ispresent(v, x) (binary_search(all(v), x))
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define repr(i, a, b) for (int i = (a); i >= (b); --i)
+#define ff first
+#define ss second
+#define mod 1000000007
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+
+void solve(){
+int n;
+cin>>n;
+vi w(n);
+rep(i,0,n){
+    cin>>w[i];
+}
+
+mii mp;
+int sum=0;
+rep(i,0,n){
+    sum+=w[i];
+    mp[sum]=i;
+}
+int s=0;
+int count=0;
+int ans=0;
+repr(i,n-1,0){
+s+=w[i];
+count++;
+if(mp.find(s)!=mp.end()){
+    int x=mp[s];
+    if(x<n-count){
+        ans=max(ans,x+count+1);
+    }
+}
+
+}
+cout<<ans<<endl;
+
+
+
+
+
+
+}
+
+
+int main() {
+    fastio();
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
