e19038657cfe178e6b84b9b310cf5642364a3a7c
priyanshu026922 <btech10886.23@bitmesra.ac.in>
2025-09-22 19:09:50 +0530

    CF1100

---
+++ B_Subtract_Operation.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define maxe(v) (*max_element(all(v)))
+#define mine(v) (*min_element(all(v)))
+#define sumv(v) (accumulate(all(v), 0LL))
+#define lb(v, x) (lower_bound(all(v), x) - (v).begin())
+#define ub(v, x) (upper_bound(all(v), x) - (v).begin())
+#define ispresent(v, x) (binary_search(all(v), x))
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define repr(i, a, b) for (int i = (a); i >= (b); --i)
+#define ff first
+#define ss second
+#define mod 1000000007
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+
+void solve(){
+ll n,k;
+cin>>n>>k;
+vll a(n);
+mll m;
+rep(i,0,n){
+    cin>>a[i];
+    m[a[i]]++;
+}
+
+
+
+rep(i,0,n){
+    if(m.find(a[i]-k)!=m.end()){
+cout<<"YES"<<endl;
+return;
+    }
+}
+cout<<"NO"<<endl;
+
+
+
+
+
+
+}
+
+
+int main() {
+    fastio();
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
