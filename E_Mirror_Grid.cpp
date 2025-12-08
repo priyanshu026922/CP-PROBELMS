9c02e87a946ede99d7861fe6c42f53ab3efe936d
priyanshu026922 <btech10886.23@bitmesra.ac.in>
2025-12-08 13:05:19 +0530

    1200 CP31

---
+++ E_Mirror_Grid.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define maxe(v) (*max_element(all(v)))
+#define mine(v) (*min_element(all(v)))
+#define sumv(v) (accumulate(all(v), 0LL))
+#define lb(v, x) (lower_bound(all(v), x) - (v).begin())
+#define ub(v, x) (upper_bound(all(v), x) - (v).begin())
+#define ispresent(v, x) (binary_search(all(v), x))
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define repr(i, a, b) for (int i = (a); i >= (b); --i)
+#define ff first
+#define ss second
+#define mod 1000000007
+ 
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+// bool cmp(pair<ll,ll>&p1,pair<ll,ll>&p2){
+//     if(p1.first==p2.first){
+//         return p1.second<p2.second;
+//     }
+ 
+//     return p1.first>p2.first;
+// }
+
+ 
+void solve(){
+int n;
+cin>>n;
+vector<vector<char>>a(n,vector<char>(n));
+rep(i,0,n){
+    rep(j,0,n){
+        cin>>a[i][j];
+    }
+}
+  int sum=0;
+for(int i=0;i<n;i++){
+    for(int j=i;j<=n-i-2;j++){
+        vector<int>q(2);
+        int x=a[i][j]-'0';
+        int y=a[j][n-i-1]-'0';
+        int z=a[n-i-1][n-j-1]-'0';
+        int m=a[n-j-1][i]-'0';
+        q[x]++;
+        q[y]++;
+        q[z]++;
+        q[m]++;
+        sum+=min(q[0],q[1]);
+    }
+}
+cout<<sum<<endl;
+
+}
+
+
+ 
+ 
+int main() {
+    fastio();
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
