49ec487266b8cb91cedebd3dc4fef3e4d2114d93
priyanshu026922 <btech10886.23@bitmesra.ac.in>
2025-12-09 22:52:20 +0530

    1200 CP31

---
+++ B_Stone_Age_Problem.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define maxe(v) (*max_element(all(v)))
+#define mine(v) (*min_element(all(v)))
+#define sumv(v) (accumulate(all(v), 0LL))
+#define lb(v, x) (lower_bound(all(v), x) - (v).begin())
+#define ub(v, x) (upper_bound(all(v), x) - (v).begin())
+#define ispresent(v, x) (binary_search(all(v), x))
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define repr(i, a, b) for (int i = (a); i >= (b); --i)
+#define ff first
+#define ss second
+#define mod 1000000007
+ 
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+// bool cmp(pair<ll,ll>&p1,pair<ll,ll>&p2){
+//     if(p1.first==p2.first){
+//         return p1.second<p2.second;
+//     }
+ 
+//     return p1.first>p2.first;
+// }
+
+ 
+void solve(){
+ll n,q;
+cin>>n>>q;
+vpll a(n,{0,0});
+rep(i,0,n){
+    cin>>a[i].first;
+}
+pll globalVal={0,-1};
+ll sum=0;
+rep(i,0,n){
+  sum+=a[i].first;
+}
+
+for (int j= 1; j <= q;j++){
+   ll t;
+    cin>>t;
+     
+    if(t==1){
+        ll i,x;
+        cin>>i>>x;
+        i--;
+        if(a[i].second>globalVal.second){
+            sum+=(x-a[i].first);
+        }else{
+            sum+=(x-globalVal.first);
+        }
+        a[i].first=x;
+        a[i].second=j;
+    }else{
+        ll x;
+        cin>>x;
+        globalVal.first=x;
+        globalVal.second=q;
+        sum=1LL*x*n;
+    }
+ cout<<sum<<endl;
+}
+
+}
+
+
+ 
+ 
+int main() {
+    fastio();
+    int t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
