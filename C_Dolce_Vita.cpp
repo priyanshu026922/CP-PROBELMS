7c22ee2b24179e3c9cdb630111a7f22b0299a64c
priyanshu026922 <btech10886.23@bitmesra.ac.in>
2025-12-10 13:35:27 +0530

    1200 CP31

---
+++ C_Dolce_Vita.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define maxe(v) (*max_element(all(v)))
+#define mine(v) (*min_element(all(v)))
+#define sumv(v) (accumulate(all(v), 0LL))
+#define lb(v, x) (lower_bound(all(v), x) - (v).begin())
+#define ub(v, x) (upper_bound(all(v), x) - (v).begin())
+#define ispresent(v, x) (binary_search(all(v), x))
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define repr(i, a, b) for (int i = (a); i >= (b); --i)
+#define ff first
+#define ss second
+#define mod 1000000007
+ 
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+// bool cmp(pair<ll,ll>&p1,pair<ll,ll>&p2){
+//     if(p1.first==p2.first){
+//         return p1.second<p2.second;
+//     }
+ 
+//     return p1.first>p2.first;
+// }
+
+ll bs(ll val,int ind,ll x){
+    ll l=1;
+    ll h=1e9+5;
+    ll ans=0;
+    while(l<=h){
+       ll mid=l+(h-l)/2;
+       if(val+(1LL*(mid-1)*(ind+1))<=x){
+          ans=mid;
+          l=mid+1;
+        }else{
+            h=mid-1;
+        }
+    }
+    return ans;
+}
+
+
+void solve(){
+ll n,x;
+cin>>n>>x;
+
+
+vll a(n);
+rep(i,0,n){
+    cin>>a[i];
+}
+
+sort(begin(a),end(a));
+vector<ll>pref(n+1);
+pref[0]=a[0];
+for(int i=1;i<n;i++){
+    pref[i]=pref[i-1]+a[i];
+}
+
+ll ans=0;
+for(int i=0;i<n;i++){
+    ans+=bs(pref[i],i,x);
+}
+cout<<ans<<endl;
+}
+
+
+ 
+ 
+int main() {
+    fastio();
+    int t;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
