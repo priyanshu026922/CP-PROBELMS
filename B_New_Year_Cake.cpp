df4adc12236236772437556fe0730ee974c31917
priyanshu026922 <btech10886.23@bitmesra.ac.in>
2025-12-31 19:58:49 +0530

    DIV2 CF

---
+++ B_New_Year_Cake.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define maxe(v) (*max_element(all(v)))
+#define mine(v) (*min_element(all(v)))
+#define sumv(v) (accumulate(all(v), 0LL))
+#define lb(v, x) (lower_bound(all(v), x) - (v).begin())
+#define ub(v, x) (upper_bound(all(v), x) - (v).begin())
+#define ispresent(v, x) (binary_search(all(v), x))
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define repr(i, a, b) for (int i = (a); i >= (b); --i)
+#define ff first
+#define ss second
+#define mod 1000000007
+ 
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+// bool cmp(pair<ll,ll>&p1,pair<ll,ll>&p2){
+//     if(p1.first==p2.first){
+//         return p1.second<p2.second;
+//     }
+ 
+//     return p1.first>p2.first;
+// }
+
+
+
+ll solve(ll a,ll b,bool top){
+ ll w=0;
+ ll d=0;
+ ll size=1;
+ bool wt=top;
+ ll layers=0;
+ while(true){
+    if(wt){
+        if(w+size>a){
+            break;
+        }
+        w+=size;
+    }else{
+            if(d+size>b){
+            break;
+            }
+        d+=size;
+    }
+     size=2*size;
+    layers++;
+     wt=!wt;
+}
+
+return layers;
+}
+ 
+ 
+ 
+ 
+int main() {
+    fastio();
+    int t;
+    cin >> t;
+    while (t--) {
+        ll a,b;
+        cin>>a>>b;
+
+        cout<<max(solve(a,b,true),solve(a,b,false))<<endl;
+    }
+    return 0;
+}
